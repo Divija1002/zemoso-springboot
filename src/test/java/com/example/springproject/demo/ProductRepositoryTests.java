38eb01218670640c8b6565a91e6a3a5724fdc9c4
Divija1002 <divija.amaram@gmail.com>
2022-05-10 10:05:03 +0530

    second commit

---
+++ src/test/java/com/example/springproject/demo/ProductRepositoryTests.java
@@ -0,0 +1,84 @@
+package com.example.springproject.demo;
+
+import com.example.springproject.demo.entity.Category;
+import com.example.springproject.demo.entity.Product;
+import com.example.springproject.demo.repository.CategoryRepository;
+import com.example.springproject.demo.repository.ProductRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ProductRepositoryTests
+{
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    @Order(1)
+    public void saveProductTest()
+    {
+        Category category=new Category("meat");
+        categoryRepository.save(category);
+        Product product=new Product();
+        product.setName("chicken");
+        product.setWeight(1);
+        product.setPrice(200);
+        product.setCategory(category);
+        productRepository.save(product);
+        Assertions.assertThat(product.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(2)
+    public void getProductTest()
+    {
+        Product product=productRepository.findById(1).get();
+        Assertions.assertThat(product.getId()).isEqualTo(1);
+    }
+
+    @Test
+    @Order(3)
+    public void getListOfProductTest()
+    {
+        List<Product> productList=productRepository.findAll();
+        Assertions.assertThat(productList.size()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(4)
+    public void updateProductTest()
+    {
+        Product product=productRepository.findById(1).get();
+        product.setName("fish");
+        Product updatedProduct=productRepository.findById(1).get();
+        Assertions.assertThat(updatedProduct.getName()).isEqualTo("fish");
+    }
+
+    @Test
+    @Order(5)
+    public void deleteProductTest()
+    {
+        productRepository.deleteById(1);
+        Optional<Product> result=productRepository.findById(1);
+        Product product=null;
+        if(result.isPresent())
+        {
+            product=result.get();
+        }
+        Assertions.assertThat(product).isNull();
+    }
+}
