911efa1b432e6ef448b2ed019ea9b8e7b4c0fffa
divija-zemoso <divija.amaram@zemosolabs.com>
2023-02-04 23:15:28 +0530

    changed category and product repository tests

--- src/test/java/com/example/springproject/demo/repositoryTests/CategoryRepositoryTests.java
+++ src/test/java/com/example/springproject/demo/repositoryTests/CategoryRepositoryTests.java
@@ -8,15 +8,15 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
-@SpringBootTest(classes = CategoryRepositoryTests.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CategoryRepositoryTests
 {
@@ -25,6 +25,7 @@ class CategoryRepositoryTests
 
     @Test
     @Order(1)
+    @Commit
     void saveCategoryTest()
     {
         Category category=new Category("meat");
