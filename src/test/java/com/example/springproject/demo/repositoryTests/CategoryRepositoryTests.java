985d56a5b7d1215b6baf062d5fc333a76bdc25c1
divija-zemoso <divija.amaram@zemosolabs.com>
2023-02-04 21:03:50 +0530

    fixed error

--- src/test/java/com/example/springproject/demo/repositoryTests/CategoryRepositoryTests.java
+++ src/test/java/com/example/springproject/demo/repositoryTests/CategoryRepositoryTests.java
@@ -10,10 +10,11 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 import java.util.Optional;
-
+@SpringBootTest(classes = CategoryRepositoryTests.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
--- src/test/java/com/example/springproject/demo/repositoryTests/ProductRepositoryTests.java
+++ src/test/java/com/example/springproject/demo/repositoryTests/ProductRepositoryTests.java
@@ -12,10 +12,12 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 import java.util.Optional;
 
+@SpringBootTest(classes = ProductRepositoryTests.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
